c45cf133cd63cbe741a3b848a9da9bf2ef1ea3b9
Farkhod95 <misirovfarkhod@gmail.com>
2025-12-11 15:36:48 +0500

    WWW

--- docoborot/models.py
+++ docoborot/models.py
@@ -4,5 +4,33 @@ from django.utils.translation import gettext_lazy as _
 
 from directory.models import Region, District, Country
 from restapp.models import BaseModel
+from users.models import Company
+
 User = settings.AUTH_USER_MODEL
 
+
+class Command(BaseModel):
+    company = models.ForeignKey(Company, related_name='command_company', on_delete=models.SET_NULL, null=True, blank=True, help_text=_("Kompaniya"))
+    order_number = models.CharField(_('Order number'), max_length=100, null=True, blank=True, help_text=_("Buyruq raqami"))
+    basis = models.CharField(_('Basis '), max_length=255, null=True, blank=True, help_text=_("Hujjat uchun asos"))
+    comment  = models.TextField(_('Comment'), blank=True, help_text=_("Izoh"))
+
+    class Meta:
+        verbose_name = _('Command')
+        verbose_name_plural = _('Commands')
+
+    def __str__(self):
+        return self.order_number
+
+
+class CommandFile(BaseModel):
+    order_document = models.ForeignKey(Command, on_delete=models.CASCADE, related_name="files", verbose_name="Order document",)
+    title = models.CharField("Title", max_length=255)
+    file = models.FileField("File", upload_to="order_documents/")
+
+    class Meta:
+        verbose_name = "Command file"
+        verbose_name_plural = "Command files"
+
+    def __str__(self):
+        return f"{self.order_document.order_number} - {self.title}"
