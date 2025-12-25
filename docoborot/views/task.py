cb00a26773daa0925b5f55c51e862aacf115d7f8
Farkhod95 <misirovfarkhod@gmail.com>
2025-12-25 11:29:07 +0500

    WWW

--- docoborot/views/task.py
+++ docoborot/views/task.py
@@ -62,15 +62,14 @@ class TaskView(ListCreateAPIView):
         user = self.request.user
         qs = Task.objects.all()
 
-        # if self._has_role(user, "Performer"):
-        #     # user object emas, id bilan filter qilish ham aniqroq bo'ladi
-        #     return qs.filter(parts__assignee_id=user.id).distinct()
-        #
-        # if self._has_role(user, "Signatory"):
-        #     return qs.filter(signed_by_id=user.id)
+        if self._has_role(user, "Performer"):
+            # user object emas, id bilan filter qilish ham aniqroq bo'ladi
+            return qs.filter(parts__assignee_id=user.id).distinct()
 
-        # return qs
-        return qs.filter(parts__assignee_id=user.id).distinct()
+        if self._has_role(user, "Signatory"):
+            return qs.filter(signed_by_id=user.id)
+
+        return qs
 
     def post(self, request):
         serializer = TaskSerializer(data=request.data)
