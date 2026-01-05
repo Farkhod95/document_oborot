d9cc04206de47649666caf3a653d0e321d20bb51
Farkhod95 <misirovfarkhod@gmail.com>
2026-01-05 18:09:11 +0500

    WWW

--- main/settings.py
+++ main/settings.py
@@ -238,7 +238,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # ===== EMAIL (Gmail SMTP) =====
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
